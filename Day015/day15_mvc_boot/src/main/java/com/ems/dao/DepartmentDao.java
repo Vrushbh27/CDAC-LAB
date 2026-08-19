ca9aba99c4b5603c5f6386ed76ad9c714b9dd2d4
Vrushbh27 <vrushbh2002@gmail.com>
2026-08-19 16:20:28 +0530

    Delete Day015 directory

--- Day015/day15_mvc_boot/src/main/java/com/ems/dao/DepartmentDao.java
+++
@@ -1,9 +0,0 @@
-package com.ems.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ems.entities.Department;
-
-public interface DepartmentDao extends JpaRepository<Department, Long> {
-    
-}
